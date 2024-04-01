8017a95493d41c1ffd96229c1c2f2c032c4bc1a3
A-23 <A-23@A-23>
2024-04-01 10:41:42 +0700

    Inisialisasi awal

---
+++ src/main/java/com/mycompany/latihan3/Latihan3.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan3;
+
+/**
+ *
+ * @author A-23
+ */
+public class Latihan3 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
